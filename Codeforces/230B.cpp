85e7157b478f27d86ded154fa38ee16d4822aa05
DT3264 <cernadaniel32@gmail.com>
2018-12-15 10:29:28 -0600

    Algorithm updated

--- Codeforces/230B.cpp
+++ Codeforces/230B.cpp
@@ -1,41 +1,47 @@
-#include <iostream>
-#include <math.h>
-
+///Knowing that the numbers with 3 divisors are the square of the primes
+///here are preloaded the primes up to 1,000,000 and their squares in a vector,
+///then for each n we read, with a binary search we look for n, and if it exist, "YES" is printed, otherwise "NO"
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<iostream>
 using namespace std;
-
-bool criba[10000001];
-int cantTPrimes;
-long long i, j;
-
-int n;
-bool isPPrime;
-long long t;
-long long sqrtT;
-
+bool used[1000001];
+vector<long long> tPrimes;
 void loadCrib(){
-    criba[0]= true;
-    criba[1]=true;
-    for(i=2; i<10000001; i++){
-        if(!criba[i]){
-            //arrTPrimes[cantTPrimes++] = i*i;
-            for(j=i*i; j<10000001; j+=i){
-                criba[j]=true;
+    used[0]=used[1]=1;
+    for(long long i=2; i<=1000000; (i==2 ? i++ : i+=2)){
+        if(!used[i]){
+            tPrimes.push_back(i*i);
+            for(long long j=i*i; j<=1000000; j+=i){
+                used[j]=true;
             }
         }
     }
 }
 
 int main(){
+    ///ios::sync_with_stdio(false);
     loadCrib();
+    int i, n;
     cin >> n;
+    long long t;
     for(i=0; i<n; i++){
         cin >> t;
-        isPPrime = false;
-        sqrtT = (long long)sqrt(t);
-        if(t == sqrtT*sqrtT){
-            if(!criba[sqrtT]) isPPrime = true;
+        int left=0;
+        int right=tPrimes.size()-1;
+        int mid;
+        bool ans=false;
+        while(left<=right){
+            mid=(left+right)/2;
+            if(tPrimes[mid]==t){
+                ans=true;
+                break;
+            }
+            else if(tPrimes[mid]>t) right=mid-1;
+            else if(tPrimes[mid]<t) left=mid+1;
         }
-        cout << (isPPrime ? "YES" : "NO") << "\n";
+        cout << (ans ? "YES\n" : "NO\n");
     }
     return 0;
 }
