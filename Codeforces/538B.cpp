5e9f47116863ba83ac7447639ce35452c735000c
DT3264 <cernadaniel@gmail.com>
2020-03-07 09:33:07 -0600

    Added dp solution

--- Codeforces/538B.cpp
+++ Codeforces/538B.cpp
@@ -1,44 +1,52 @@
-#include<stdio.h>
-int arr[10];
-int digits;
-int cantN;
-
-void reverseArr(int toReverse[]){
-    int tArr[10];
-    int i;
-    for(i=0; i<digits; i++){
-        tArr[i]=arr[digits-i-1];
-    }
-    for(i=0; i<digits; i++){
-        arr[i]=tArr[i];
-    }
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int dp[1000001];
+int prevN[1000001];
+vector<int> quasi;
+string num="0000000";
+int getNum(){
+    return stoi(num);
 }
-
-int main (){
-    int n, i, j;
-    scanf("%d", &n);
-    while(n>0){
-        if(cantN<n%10){
-            cantN=n%10;
+void generateQuasi(int pos){
+    if(pos==num.size()){
+        int num=getNum();
+        if(num>0 && num<=n){
+            quasi.push_back(num);
         }
-        arr[digits++]=n%10;
-        n/=10;
+        return;
     }
-    reverseArr(arr);
-    printf("%d\n", cantN);
-    bool canPrint;
-    for(i=0; i<cantN; i++){
-        canPrint=false;
-        for(j=0; j<digits; j++){
-            if(arr[j]>0 && !canPrint){
-                canPrint=true;
+    num[pos]='0';
+    generateQuasi(pos+1);
+    num[pos]='1';
+    generateQuasi(pos+1);
+}
+int main(){
+    cin >> n;
+    generateQuasi(0);
+    memset(dp, -1, sizeof(dp));
+    memset(prevN, -1, sizeof(prevN));
+    dp[0]=0;//Marco mi estado inicial, si no, ninguno de delante puede seguir
+    for(int i=1; i<=n; i++){
+        for(int x : quasi){
+            //i-x>=0: Con X puedo llegar hasta I o aproximarme sin pasarme
+        	//dp[i]==-1: no he llegado ahí antes
+        	//dp[i-x]+1 < dp[i]: si dado lo mejor en mi pos anterior + 1
+        	//llego con algo mejor a lo que tengo
+            if(i-x>=0 && (dp[i-x]+1<dp[i] || dp[i]==-1)){
+                dp[i]=dp[i-x]+1;
+                prevN[i]=i-x;
             }
-            if(canPrint){
-                printf("%d", (arr[j]>=1 ? 1 : 0));
-            }
-            arr[j]--;
         }
-        printf(" ");
+    }
+    vector<int> ans;
+    while(n>0){
+        ans.push_back(n-prevN[n]);
+        n=prevN[n];
+    }
+    cout << ans.size() << endl;
+    for(int x : ans){
+        cout << x << " ";
     }
     return 0;
 }
